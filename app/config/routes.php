ce16c48a4f80e8e5133d30dac8db513b3fec7b89
mickael couzinet <mcouzinet@gmail.com>
2014-10-25 18:12:39 +0200

    hello world

---
+++ app/config/routes.php
@@ -0,0 +1,8 @@
+<?php
+
+use lithium\net\http\Router;
+use lithium\core\Environment;
+
+Router::connect('/', 'Travelight::index');
+
+?>
---
+++ app/controllers/TravelightController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\controllers;
+ 
+class TravelightController extends \lithium\action\Controller {
+
+	public function index() {
+		
+		return $this->render();
+	}
+}
+
+?>
